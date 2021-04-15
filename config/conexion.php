cbf42a6bf10ca1cb8c29cf6f48b60def619fa5de
{jhasmany-jhunnior} <{jhasmany_jhunnior@outlook.com}>
2021-04-15 19:07:50 -0400

    probando subiendo a la nube3

--- config/conexion.php
+++ config/conexion.php
@@ -6,8 +6,18 @@
 
         protected function Conexion(){
             try {
-				$conectar = $this->dbh = new PDO("mysql:local=us-cdbr-east-03.cleardb.com;dbname=heroku_d8f3c9feb53c652","b7ec4239a92046","5df2ec71");
-				return $conectar;	
+				/*$conectar = $this->dbh = new PDO("mysql:local=us-cdbr-east-03.cleardb.com;dbname=heroku_d8f3c9feb53c652","b7ec4239a92046","5df2ec71");
+				return $conectar;*/
+                
+                $url = parse_url(getenv("https://www.cleardb.com/database/details?id=0C3723A2AFBABF7B5D5209AD1BBB1B74"));
+
+                $server = $url["us-cdbr-east-03.cleardb.com"];
+                $username = $url["b7ec4239a92046"];
+                $password = $url["5df2ec71"];
+                $db = substr($url["heroku_d8f3c9feb53c652"], 1);
+
+                $conn = new mysqli($server, $username, $password, $db);
+
 			} catch (Exception $e) {
 				print "¡Error BD!: " . $e->getMessage() . "<br/>";
 				die();	
