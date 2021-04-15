c7affebdf1d2c03a497bd3731fde75f21e8a2859
{jhasmany-jhunnior} <{jhasmany_jhunnior@outlook.com}>
2021-04-15 19:36:36 -0400

    probando subiendo a la nube5

--- config/conexion.php
+++ config/conexion.php
@@ -9,7 +9,7 @@
 				/*$conectar = $this->dbh = new PDO("mysql:local=us-cdbr-east-03.cleardb.com;dbname=heroku_d8f3c9feb53c652","b7ec4239a92046","5df2ec71");
 				return $conectar;*/
                 
-                $url = parse_url(getenv("https://www.cleardb.com/database/details?id=0C3723A2AFBABF7B5D5209AD1BBB1B74"));
+                $url = parse_url(getenv("mysql://b7ec4239a92046:5df2ec71@us-cdbr-east-03.cleardb.com/heroku_d8f3c9feb53c652?reconnect=true"));
 
                 $server = $url["us-cdbr-east-03.cleardb.com"];
                 $username = $url["b7ec4239a92046"];
